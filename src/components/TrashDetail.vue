5086cf09c253a9f5f3effe2fe2d376608e6dc67f
Guang-Yu6 <everyting-day@foxmail.com>
2022-05-08 18:37:18 +0800

    头像

---
+++ src/components/TrashDetail.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>
+      {{msg}}
+    </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TrashDetail",
+  data(){
+    return {
+      msg:'回收站笔记详情页'
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1 {
+  color:palegoldenrod;
+}
+</style>
